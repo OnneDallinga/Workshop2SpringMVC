298bd35d2f2da098e6cbed1239b7c2a21d6b53e4
= <=>
2018-05-01 18:00:28 +0200

    Test

---
+++ Workshop2SpringMVC/src/main/java/WIneMain/Workshop2SpringMvcApplication.java
@@ -0,0 +1,14 @@
+package WIneMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@SpringBootApplication
+public class Workshop2SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop2SpringMvcApplication.class, args);
+	}
+}
