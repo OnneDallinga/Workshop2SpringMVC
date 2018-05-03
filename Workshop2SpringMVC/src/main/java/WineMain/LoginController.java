0647f7520236754b350c5920a6d79f1c3e605583
= <=>
2018-05-03 14:00:05 +0200

    Removed @requestmapping

--- Workshop2SpringMVC/src/main/java/WineMain/LoginController.java
+++ Workshop2SpringMVC/src/main/java/WineMain/LoginController.java
@@ -4,14 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import utility.ControllerInterface;
  
 
 @Slf4j
 @Controller
-@RequestMapping("/login") 
 public class LoginController implements ControllerInterface {
 	
 	@GetMapping("/login")
