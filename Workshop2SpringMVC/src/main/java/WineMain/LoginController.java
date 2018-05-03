ed6046b8c7bf114d5ee265ce246019c73d6cad69
= <=>
2018-05-03 14:20:01 +0200

    Added basic security

--- Workshop2SpringMVC/src/main/java/WineMain/LoginController.java
+++ Workshop2SpringMVC/src/main/java/WineMain/LoginController.java
@@ -1,20 +1,30 @@
 package WineMain;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.validation.Valid;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import account.Account;
 import lombok.extern.slf4j.Slf4j;
 import utility.ControllerInterface;
- 
 
 @Slf4j
 @Controller
 public class LoginController implements ControllerInterface {
-	
+
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
-}
 
+	@PostMapping
+	public String processDesign(@Valid Account account, Errors errors) {
+		if (errors.hasErrors()) {
+			return "account";
+		}
+		return "redirect:/mainpage";
+	}
+}
