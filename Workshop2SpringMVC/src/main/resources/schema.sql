9ed5311b7a0aa75da92dfc51dbf067579d105e2f
= <=>
2018-05-07 14:14:06 +0200

    Added unique to schema.sql

--- Workshop2SpringMVC/src/main/resources/schema.sql
+++ Workshop2SpringMVC/src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 create table if not exists account (
-	id bigint not null,
-	username varchar(25) not null,
+	id bigint not null unique,
+	username varchar(25) not null unique,
 	encryptedpassword varchar(50) not null,
 	enabled bit DEFAULT 1,
 	salt varchar(50),
 	date time
 );
 create table if not exists authorities (
-	id bigint not null,
-	username varchar(25) not null,
+	id bigint not null unique,
+	username varchar(25) not null unique,
 	authority varchar(10) not null
 );
