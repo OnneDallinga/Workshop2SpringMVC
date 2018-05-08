f4c1bb387ef36ebe21ed235f77b79816f9fcbd5b
Geert Lamers <wickeh@gmail.com>
2018-05-08 15:16:52 +0200

    fixed some schema structuring due to BCrypt
    BCrypt stores hash as String with set length 60, the salt is embedded within the hash

--- Workshop2SpringMVC/src/main/resources/schema.sql
+++ Workshop2SpringMVC/src/main/resources/schema.sql
@@ -1,11 +1,10 @@
 create table if not exists account (
 	id bigint not null unique,
 	username varchar(25) not null unique,
-	encrypted_password varchar(50) not null,
+	encrypted_password char(60) not null,
 	enabled bit DEFAULT 1,
-	salt varchar(50),
 	created_on timestamp,
-	last_modified_on timestamp
+	last_modified_on timestamp DEFAULT current_timestamp
 );
 create table if not exists authorities (
 	id bigint not null unique,
