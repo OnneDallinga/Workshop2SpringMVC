97a34fab924fe0812edb58618bbeb7238aeec857
Geert Lamers <wickeh@gmail.com>
2018-05-21 21:44:48 +0200

    fixed login page allowing anonymous resource access

--- Workshop2SpringMVC/src/main/java/wine/configuration/SecurityConfig.java
+++ Workshop2SpringMVC/src/main/java/wine/configuration/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,22 +19,22 @@ import wine.repositories.AccountRepository;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private AccountRepository accountRepo;
 
 	@Autowired
 	DataSource dataSource;
-	
+
 	@Bean
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder(8); // set to strength:8 for 8 encryption rounds
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth)
 	    throws Exception {
-  
+
 	  auth
 	    .jdbcAuthentication()
 	      .dataSource(dataSource)
@@ -45,14 +46,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		      "where username=?")
 	      .passwordEncoder(encoder());; // the encryption
 	}
-	
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/images/**");
+    }
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 	  http
-	    .authorizeRequests()
-	    	.antMatchers("/register").permitAll()
-	    	.anyRequest().authenticated()
-	    	.and()
+              .anonymous().and()
+	             .	authorizeRequests()
+	    	        .antMatchers("/register").permitAll()
+			        .antMatchers("/webjars/**").permitAll()
+			        .anyRequest().authenticated()
+              .and()
 	    .formLogin()
 	        .loginPage("/login") // defines login page
 	        .defaultSuccessUrl("/home", true) // defines where user is forced to go after logging in (true)
@@ -60,7 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	        .and()
 	        .logout()
 	        	.logoutUrl("/logout")
-	        	.logoutSuccessUrl("/login") // logout page                             
+	        	.logoutSuccessUrl("/login") // logout page
 				.invalidateHttpSession(true)
 	        ;
 	}
